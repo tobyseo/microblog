a17fa8da170e4db6ee8efd6a67f9bb9f09c9b6cb
Toby Seo <tobyslow@gmail.com>
2016-02-03 17:54:09 +0900

    ..

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
+import os
+
 WTF_CSRF_ENABLED = True
 SECRET_KEY = r'-^vr^+^mc6r_v#(orjbeu$#77050ke4lo-bt%xuyl5bd%wrnvv'
 
@@ -7,3 +9,8 @@ OPENID_PROVIDERS = [
     {'name': 'AOL', 'url': 'http://openid.aol.com/<username>'},
     {'name': 'Flickr', 'url': 'http://www.flickr.com/<username>'},
     {'name': 'MyOpenID', 'url': 'https://www.myopenid.com'}]
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
